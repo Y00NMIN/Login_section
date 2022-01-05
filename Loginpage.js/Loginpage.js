a7443b68d4d95071758460628638a7376d2a53e4
SungMin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2022-01-05 14:18:09 +0000

    LoginPage-Submit

--- Loginpage.js/Loginpage.js
+++ Loginpage.js/Loginpage.js
@@ -12,13 +12,19 @@ function LoginPage() {
 		setPassword(event.currenTarget.value)
 	}
 
+	const onSubmitHandler= (event) =>{
+		event.preventDefault(); //페이지 미 새로고침 구문
+	}
+
 	
 	return(
 		<div style{{
 		display: 'flex', justifyContent:'center', alignItems:'center',width:'100%', height: '100vh'
 		}}>
 
-			<form style={{display:'flex', flexDirection:'column'}}>
+			<form style={{display:'flex', flexDirection:'column'}}
+				onSubmit={onSubmitHandler}
+			>
 				<label>Email</label>
 				<input type="email" value={Email} onChange={onEmailHandler} />
 				<label>Password</label>
